8ea2ae3c7f7759a1b73cf6bedafc976e49847566
PrestonHunter <prestonhunter2018@gmail.com>
2023-03-01 12:14:38 -0700

    have alternating display on click, and search implemented

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokeList, setPokeList, searchTerm}) {
+  const [isLoaded, setIsLoaded] = useState(false)
+  useEffect(()=>{
+    fetch("http://localhost:3001/pokemon").then(resp=>resp.json()).then(arr=>{setPokeList(arr); setIsLoaded(true)})
+  },[])
+
+  const filteredList = pokeList.filter((pokemon)=>{
+    if(pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())){
+      return true;
+    }
+    return false;
+  })
+
+  if (!isLoaded){return <h2>loading</h2>}
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {filteredList.map((pokemon)=>{return <PokemonCard name = {pokemon.name} hp = {pokemon.hp} frontSprite={pokemon.sprites.front} backSprite={pokemon.sprites.back}></PokemonCard>})}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokeList, setPokeList] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection searchTerm={searchTerm} pokeList={pokeList} setPokeList={setPokeList}/>
     </Container>
   );
 }
